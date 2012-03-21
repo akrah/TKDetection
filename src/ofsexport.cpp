6c4c734b9f3a2585a6f350d8670f00fc4e1936dd
Adrien Krähenbühl <adrien.krahenbuhl@loria.fr>
2012-03-21 10:48:59 +0100

    Fin du merge.

--- src/ofsexport.cpp
+++ src/ofsexport.cpp
@@ -19,20 +19,21 @@ namespace OfsExport {
 		void computeEgesLinks( const int &nbEdges, const int &nbSlices, QTextStream &stream );
 	}
 
-  void process( const Billon &billon, const Marrow &marrow, const SlicesInterval &interval, const QString &fileName, const int &nbEdgesPerSlice, const int &radiusOfTubes ) {
-	  if ( interval.containsClosed(marrow.interval()) ) {
-	    QFile file(fileName);
-	    if ( file.open(QIODevice::WriteOnly) ) {
-	      QTextStream stream(&file);
-	      stream << "OFS MHD" << endl;
-	      computeAllEdges( billon, marrow, interval, nbEdgesPerSlice, radiusOfTubes, stream );
-	      computeEgesLinks( nbEdgesPerSlice, interval.count(), stream );
-	      
-	      file.close();
-	    }
-	  }else{
-	    std::cerr << "Saving not possible since the mesh is outside the SlicesInterval" << endl;
-	  }
+	void process( const Billon &billon, const Marrow &marrow, const SlicesInterval &interval, const QString &fileName, const int &nbEdgesPerSlice, const int &radiusOfTubes ) {
+		if ( interval.containsClosed(marrow.interval()) ) {
+			QFile file(fileName);
+			if ( file.open(QIODevice::WriteOnly) ) {
+				QTextStream stream(&file);
+				stream << "OFS MHD" << endl;
+				computeAllEdges( billon, marrow, interval, nbEdgesPerSlice, radiusOfTubes, stream );
+				computeEgesLinks( nbEdgesPerSlice, interval.count(), stream );
+
+				file.close();
+			}
+		}
+		else {
+			std::cerr << "Saving not possible since the mesh is outside the SlicesInterval" << std::endl;
+		}
 	}
 
 	namespace {
