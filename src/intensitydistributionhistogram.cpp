eeeec2e24a1c0a51a2735546a356ce198b2c6cab
adrien057 <adrien.krahenbuhl@loria.fr>
2013-07-08 11:44:11 +0200

    Modification du calcul de l'histogramme d'intensité dans une zone de nœuds

--- src/intensitydistributionhistogram.cpp
+++ src/intensitydistributionhistogram.cpp
@@ -1,6 +1,8 @@
 #include "inc/intensitydistributionhistogram.h"
 
 #include "inc/billon.h"
+#include "inc/piechart.h"
+#include "inc/coordinate.h"
 
 IntensityDistributionHistogram::IntensityDistributionHistogram() : Histogram<qreal>()
 {
@@ -29,6 +31,36 @@ void IntensityDistributionHistogram::construct( const Billon &billon, const Inte
 			for ( i=0 ; i<width ; ++i )
 			{
 				if ( intensityInterval.containsClosed(slice.at(j,i)) ) ++((*this)[slice.at(j,i)-minVal]);
+			}
+		}
+	}
+
+	meansSmoothing(smoothingRadius,false);
+}
+
+void IntensityDistributionHistogram::construct( const Billon &billon, const Interval<uint> &sliceInterval, const Interval<uint> &sectorInterval,
+												const PieChart &pieChart, const iCoord2D &pithCoord, const Interval<int> &intensityInterval, const uint &smoothingRadius )
+{
+	const uint &width = billon.n_cols;
+	const uint &height = billon.n_rows;
+	const int &minVal = intensityInterval.min();
+
+	uint i, j, k;
+
+	clear();
+	resize(intensityInterval.size()+1);
+
+	for ( j=0 ; j<height ; ++j )
+	{
+		for ( i=0 ; i<width ; ++i )
+		{
+
+			if ( sectorInterval.containsClosed(pieChart.sectorIndexOfAngle(pithCoord.angle(iCoord2D(i,j)))) && pithCoord.euclideanDistance(iCoord2D(i,j)) < 200 )
+			{
+				for ( k=sliceInterval.min() ; k<=sliceInterval.max() ; ++k )
+				{
+					if ( intensityInterval.containsClosed(billon.slice(k).at(j,i)) ) ++((*this)[billon.slice(k).at(j,i)-minVal]);
+				}
 			}
 		}
 	}
