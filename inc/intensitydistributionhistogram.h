6553a5b389c13732703bc0419081a4b4e8558318
adrien057 <adrien.krahenbuhl@loria.fr>
2013-07-08 11:44:11 +0200

    Modification du calcul de l'histogramme d'intensité dans une zone de nœuds

--- inc/intensitydistributionhistogram.h
+++ inc/intensitydistributionhistogram.h
@@ -2,7 +2,10 @@
 #define INTENSITYDISTRIBUTIONHISTOGRAM_H
 
 #include "def/def_billon.h"
+#include "def/def_coordinate.h"
 #include "inc/histogram.h"
+
+class PieChart;
 
 class IntensityDistributionHistogram : public Histogram<qreal>
 {
@@ -11,6 +14,8 @@ public:
 	virtual ~IntensityDistributionHistogram();
 
 	void construct( const Billon &billon, const Interval<uint> &sliceInterval, const Interval<int> &intensityInterval, const uint &smoothingRadius );
+	void construct( const Billon &billon, const Interval<uint> &sliceInterval, const Interval<uint> &sectorInterval, const PieChart &pieChart, const iCoord2D &pithCoord,
+					const Interval<int> &intensityInterval, const uint &smoothingRadius );
 };
 
 #endif // INTENSITYDISTRIBUTIONHISTOGRAM_H
