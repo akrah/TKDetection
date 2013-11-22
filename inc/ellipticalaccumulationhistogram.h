efecff7d6c40341fd2d8d3114f0828daaa0f9806
adrien057 <adrien.krahenbuhl@loria.fr>
2013-11-22 22:14:17 +0100

    - Ajout du profil de cumul elliptique d'intensité - Ajout de l'affichage des ellipses - Modification de l'interface graphique pour une meilleure intégration des coupes transversales

---
+++ inc/ellipticalaccumulationhistogram.h
@@ -0,0 +1,21 @@
+#ifndef ELLIPTICALACCUMULATIONHISTOGRAM_H
+#define ELLIPTICALACCUMULATIONHISTOGRAM_H
+
+#include "def/def_billon.h"
+#include "def/def_coordinate.h"
+
+#include "inc/histogram.h"
+
+class EllipticalAccumulationHistogram : public Histogram<qreal>
+{
+public:
+	EllipticalAccumulationHistogram();
+	virtual ~EllipticalAccumulationHistogram();
+
+	void construct( const Slice &slice, const uiCoord2D &origin, const qreal &ellipticityRate );
+
+private:
+	void findFirstMaximumAndNextMinimum();
+};
+
+#endif // ELLIPTICALACCUMULATIONHISTOGRAM_H
