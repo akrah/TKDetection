d31aa93ceab37142206f805da559d71cc32344f1
Kerautret <kerautre@loria.fr>
2013-11-26 10:22:05 +0100

    add visu patch

--- PatchVisu/visuPatches.cpp
+++ PatchVisu/visuPatches.cpp
@@ -45,11 +45,15 @@ int main(int argc, char** argv)
   po::options_description general_opt("Allowed options are: ");
   general_opt.add_options()
     ("help,h", "display this message")
-    ("volumeFile,v", po::value<std::string>(), "import volume image" )
+    ("volumeFile,v", po::value<std::string>(), "import volume image (dicom format)" )
+    ("tangentialView", "Use tangential view instead by default sectional view defined from set of points")
     ("pointsPk,p", po::value<std::string>(), "import the set of points Pk used to determine the image position." )
     ("scaleX,x",  po::value<float>()->default_value(1.0), "set the scale value in the X direction (default 1.0)" )
     ("scaleY,y",  po::value<float>()->default_value(1.0), "set the scale value in the Y direction (default 1.0)" )
     ("scaleZ,z",  po::value<float>()->default_value(1.0), "set the scale value in the Z direction (default 1.0)")
+    ("patchMaxWidth,w",  po::value<unsigned int>()->default_value(50), "set the maximal patch width (default 50)")
+    ("sampleStep,s",  po::value<unsigned int>()->default_value(20), "set the step between each patch images (default 20)")
+    ("cutEnd,s",  po::value<unsigned int>()->default_value(0), "remove some end points of the profiles. ")
     ("trunkBark-mesh,t", po::value<std::string>(), "mesh of the trunk bark in format OFS non normalized (.ofs)" )
     ("marrow-mesh,m", po::value<std::string>(), "mesh of trunk marrow  in format OFS non normalized (.ofs)" );
   
@@ -105,46 +109,80 @@ int main(int argc, char** argv)
   float sx = vm["scaleX"].as<float>();
   float sy = vm["scaleY"].as<float>();
   float sz = vm["scaleZ"].as<float>();      
+  unsigned int patchMaxWidth = vm["patchMaxWidth"].as<unsigned int>();
+  unsigned int sampleStep = vm["sampleStep"].as<unsigned int>();
+  unsigned int cutEnd = vm["cutEnd"].as<unsigned int> ();
+  
   viewer.setGLScale(sx,sy,sz);
   Image3D imageVol = DicomReader< Image3D,  RescalFCT  >::importDicom(vm["volumeFile"].as<std::string>(), RescalFCT(-900,
-                                                                                                                    0,
+                                                                                                                    530,
                                                                                                                     0, 255));
+  
   std::vector<unsigned int> indexTopLeft; indexTopLeft.push_back(3); indexTopLeft.push_back(4); indexTopLeft.push_back(5);
-  std::vector<unsigned int> indexTopRight; indexTopRight.push_back(6); indexTopRight.push_back(7); indexTopRight.push_back(8);
+  std::vector<unsigned int> indexBottomLeft; indexBottomLeft.push_back(6); indexBottomLeft.push_back(7); indexBottomLeft.push_back(8);
+  std::vector<unsigned int> indexBottomRight; indexBottomRight.push_back(9); indexBottomRight.push_back(10); indexBottomRight.push_back(11);
+  std::vector<unsigned int> indexTopRight; indexTopRight.push_back(12); indexTopRight.push_back(13); indexTopRight.push_back(14);
   
   std::vector<Z3i::RealPoint> vectPointsCenter = DGtal::PointListReader<Z3i::RealPoint>::getPointsFromFile(vm["pointsPk"].as<std::string>());
-  std::vector<Z3i::RealPoint> vectPointsLeft = DGtal::PointListReader<Z3i::RealPoint>::getPointsFromFile(vm["pointsPk"].as<std::string>(), indexTopLeft);
-  std::vector<Z3i::RealPoint> vectPointsRight = DGtal::PointListReader<Z3i::RealPoint>::getPointsFromFile(vm["pointsPk"].as<std::string>(), indexTopRight);
-  std::vector<Z3i::RealPoint>::iterator itUpL = vectPointsLeft.begin();
-  std::vector<Z3i::RealPoint>::iterator itUpR = vectPointsRight.begin();
+  std::vector<Z3i::RealPoint> vectPointsTopLeft = DGtal::PointListReader<Z3i::RealPoint>::getPointsFromFile(vm["pointsPk"].as<std::string>(), indexTopLeft);
+  std::vector<Z3i::RealPoint> vectPointsTopRight = DGtal::PointListReader<Z3i::RealPoint>::getPointsFromFile(vm["pointsPk"].as<std::string>(), indexTopRight);
+  std::vector<Z3i::RealPoint> vectPointsBottomLeft = DGtal::PointListReader<Z3i::RealPoint>::getPointsFromFile(vm["pointsPk"].as<std::string>(), indexBottomLeft);
+  std::vector<Z3i::RealPoint> vectPointsBottomRight = DGtal::PointListReader<Z3i::RealPoint>::getPointsFromFile(vm["pointsPk"].as<std::string>(), indexBottomRight);
+
+  trace.info() << "list of center point, contentains " << vectPointsCenter.size() << " points" << std::endl;
+  trace.info() << "list of top left point, contentains " << vectPointsTopLeft.size() << " points" << std::endl;
+  trace.info() << "list of top right point, contains " << vectPointsTopRight.size() << " points" << std::endl;
+  trace.info() << "list of bottom left point, contains " << vectPointsBottomLeft.size() << " points" << std::endl;
+  trace.info() << "list of bottom right point, contains " << vectPointsBottomRight.size() << " points" << std::endl;
+  
   DGtal::DefaultFunctor idV;
   unsigned k =0;
-  for (unsigned int i =0; i< vectPointsCenter.size()-15; 
+  for (unsigned int i =0; i< vectPointsCenter.size() - cutEnd; 
        i=i+1){
     viewer.setFillColor(DGtal::Color(250,20,20,255));
     viewer << vectPointsCenter.at(i);
-    if(i%10==0&& i< vectPointsCenter.size()-20){
-      unsigned int  width = (50<(vectPointsRight.at(i) - vectPointsLeft.at(i)).norm()) ? 50:
-        (vectPointsRight.at(i) - vectPointsLeft.at(i)).norm();
-      DGtal::Z2i::Domain domainImage2D (DGtal::Z2i::Point(0,0), 
-                                        DGtal::Z2i::Point(width, width)); 
+    if(i%sampleStep==0&& i< vectPointsCenter.size()-sampleStep){
       
-      DGtal::Point2DEmbedderIn3D<DGtal::Z3i::Domain >  embedder(imageVol.domain(), 
-                                                                vectPointsCenter.at(i), 
-                                                                vectPointsCenter.at(i) - vectPointsCenter.at(i+10), 
-                                                                width);
-      ImageAdapterExtractor extractedImage(imageVol, domainImage2D, embedder, idV);
-      viewer << extractedImage;
-      viewer << DGtal::UpdateImage3DEmbedding<Z3i::Space, Z3i::KSpace>(k, 
-                                                                       embedder(Z2i::RealPoint(0,0), false),
-                                                                       embedder(Z2i::RealPoint(width,0), false),
-                                                                       embedder(domainImage2D.upperBound(), false),
-                                                                       embedder(Z2i::RealPoint(0, width), false));
+      if(!vm.count("tangentialView")){
+        unsigned int  width = (patchMaxWidth<(vectPointsTopRight.at(i) - vectPointsTopLeft.at(i)).norm()) ? patchMaxWidth:
+          (vectPointsTopRight.at(i) - vectPointsTopLeft.at(i)).norm();
+        DGtal::Z2i::Domain domainImage2D (DGtal::Z2i::Point(0,0), 
+                                          DGtal::Z2i::Point(width, width)); 
+        
+        DGtal::Point2DEmbedderIn3D<DGtal::Z3i::Domain >  embedder(imageVol.domain(), 
+                                                                  vectPointsCenter.at(i), 
+                                                                  vectPointsCenter.at(i) - vectPointsCenter.at(i+sampleStep), 
+                                                                  width);
+        ImageAdapterExtractor extractedImage(imageVol, domainImage2D, embedder, idV);
+        viewer << extractedImage;
+        viewer << DGtal::UpdateImage3DEmbedding<Z3i::Space, Z3i::KSpace>(k, 
+                                                                         embedder(Z2i::RealPoint(0,0), false),
+                                                                         embedder(Z2i::RealPoint(width,0), false),
+                                                                         embedder(domainImage2D.upperBound(), false),
+                                                                         embedder(Z2i::RealPoint(0, width), false));
+        
+      }else{
+      
+        DGtal::Z2i::Domain domainImage2D (DGtal::Z2i::Point(0,0), 
+                                          DGtal::Z2i::Point((vectPointsTopRight.at(i) - vectPointsTopLeft.at(i)).norm(),
+                                                            (vectPointsTopRight.at(i) - vectPointsBottomRight.at(i)).norm())); 
+            
+         DGtal::Point2DEmbedderIn3D<DGtal::Z3i::Domain >  embedder(imageVol.domain(), 
+                                                                   vectPointsBottomLeft.at(i),vectPointsBottomRight.at(i), vectPointsTopLeft.at(i),
+                                                                   DGtal::Z3i::Point(0,0, 0));
+         ImageAdapterExtractor extractedImage(imageVol, domainImage2D, embedder, idV);        
+         viewer << extractedImage;
+         viewer << DGtal::UpdateImage3DEmbedding<Z3i::Space, Z3i::KSpace>(k, 
+                                                                          embedder(Z2i::RealPoint(0,0), false),
+                                                                          embedder(Z2i::RealPoint((vectPointsTopRight.at(i) - vectPointsTopLeft.at(i)).norm(),0), false),
+                                                                          embedder(domainImage2D.upperBound(), false),
+                                                                          embedder(Z2i::RealPoint(0, (vectPointsTopRight.at(i) - vectPointsBottomRight.at(i)).norm()), false));
+      }     
+      
       k++;
    
     }
-    itUpL++;
-    itUpR++;  
+
   } 
   
   
